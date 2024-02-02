425492c52b8606679d66f2fa2e10048bbfe4af9b
Rashaun Bennett <rashaunbennett1017@gmail.com>
2024-02-01 19:07:48 -0500

    Almost Done WEEEEEE

--- modules/legoSets.js
+++ modules/legoSets.js
@@ -1,8 +1,7 @@
 const setData = require('../data/setData');
 const themeData = require('../data/themeData');
-
+let color = require('colors');
 let sets = [];
-
 function initialize() {
   return new Promise((resolve, reject) => {
     // place our code inside a "Promise" function
@@ -10,31 +9,65 @@ function initialize() {
       ...set,
       theme: themeData.find((theme) => theme.id === set.theme_id).name,
     }));
-    resolve(); // call "resolve" because we have completed the function successfully
+    resolve(
+      `\nInitialize() Function: The "sets" array is filled with objects ✅`
+        .green
+    ); // call "resolve" because we have completed the function successfully
   });
 }
 
 function getAllSets() {
-  return Promise.resolve(sets);
+  return new Promise((resolve, reject) => {
+    if (sets.length > 0) {
+      resolve(sets);
+    } else {
+      reject('getAllSets() Function: Failed to get ALL Sets ❌'.red);
+    }
+  });
 }
 
 function getSetByNum(setNum) {
   if (sets.find((element) => element.set_num === setNum)) {
     return Promise.resolve(sets.find((element) => element.set_num === setNum));
   } else {
-    return Promise.reject('Unable to find requested set');
+    return Promise.reject('Unable to find requested set ❌'.red);
   }
 }
 
 function getSetsByTheme(theme) {
-  const result = words.filter((word) => word === 'spray');
+  const filteredSets = sets.filter((set) =>
+    set.theme.toLowerCase().includes(theme.toLowerCase())
+  );
+  if (filteredSets.length > 0) {
+    console.log('Set Found By Theme Name ✅');
+    return Promise.resolve(filteredSets);
+  } else {
+    return Promise.reject('Unable to find requested sets ❌'.red);
+  }
 }
-initialize()
-  .then((data) => console.log('initialize resolved!'))
-  .then(() => {
-    console.log(`Getting all sets...`);
-    getAllSets();
-  })
-  .then(() => getSetByNum('001-1'));
 
-// module.exports = { initialize, getAllSets, getSetByNum, getSetsByTheme };
+initialize()
+  .then((data) => console.log(data))
+  .catch(() => {
+    console.log(`Initialization Failed ❌`.red);
+  });
+
+getAllSets()
+  .then((data) => console.log(data))
+  .catch((reason) => {
+    console.log(reason);
+  });
+
+getSetByNum('001-1')
+  .then((data) => console.log(data))
+  .catch((reason) => {
+    console.log(reason);
+  });
+
+getSetsByTheme('tech')
+  .then((data) => console.log(data))
+  .catch((reason) => {
+    console.log(reason);
+  });
+
+module.exports = { initialize, getAllSets, getSetByNum, getSetsByTheme };
--- server.js
+++ server.js
@@ -1,20 +1,48 @@
+/********************************************************************************
+ * WEB322 – Assignment 02
+ *
+ * I declare that this assignment is my own work in accordance with Seneca's
+ * Academic Integrity Policy:
+ *
+ * https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
+ *
+ * Name: Rashaun Bennett Student ID: 124823220 Date: February 2nd, 2024
+ *
+ * Published URL: ___________________________________________________________
+ *
+ ********************************************************************************/
+
 const express = require('express'); // "require" the Express module
 const app = express(); // obtain the "app" object
 const dotenv = require('dotenv').config();
 const HTTP_PORT = process.env.PORT || 8080; // assign a port
 const legoData = require('./modules/legoSets');
-
+const path = require('path');
 let colors = require('colors');
 
 const studentName = 'Rashaun Bennett';
 const studentId = '124823220';
-console.log(`hello`);
+
+app.use(express.static('public'));
 
 // start the server on the port and output a confirmation to the console
-app.listen(HTTP_PORT, () =>
-  console.log(`server listening on:`.green, `${HTTP_PORT}`.blue)
-);
+app.listen(HTTP_PORT, () => {
+  console.log(`Student Name: `.yellow, `${studentName}`.green);
+  console.log(`Student ID #: `.yellow, `${studentId}`.green, '\n');
+  console.log(
+    `Server Listening On:`.green,
+    `http://localhost:${HTTP_PORT}`.blue,
+    '\n'
+  );
+});
 
 app.get('/', (req, res) => {
-  res.send(`Assignment 2: ${studentName} - ${studentId}`);
+  res.sendFile(path.join(__dirname, '/views/home.html'));
 });
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/about.html'));
+});
+
+// app.get('/', (req, res) => {
+//   res.send(`Assignment 2: ${studentName} - ${studentId}`);
+// });
