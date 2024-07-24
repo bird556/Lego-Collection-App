72c82fdb1e33686348fa56a96ae18c1100eb5da3
Rashaun B <rbennett17@myseneca.ca>
2024-07-23 20:13:47 -0400

    update

--- api/server.js
+++ api/server.js
@@ -23,7 +23,8 @@ const github = '@bird556';
 })();
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + 'public'));
+app.set('views', path.join(__dirname, '../views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
