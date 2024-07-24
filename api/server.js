c7ce4e5d34715cc7f06cb4308bb256f9aeeb052a
Rashaun B <rbennett17@myseneca.ca>
2024-07-23 20:07:19 -0400

    changed express.static

--- api/server.js
+++ api/server.js
@@ -23,7 +23,7 @@ const github = '@bird556';
 })();
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(__dirname + 'public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
